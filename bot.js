d435ca064bff03a7cc30c779b255fd1b30a65194
spaminggbots <44527121+spaminggbots@users.noreply.github.com>
2018-10-27 18:13:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,4 +48,18 @@ msg.delete();
 })
 }
 });
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`California Shop`,"http://twitch.tv/Cypher Bot")
+client.user.setStatus("dnd")
+});
+
+
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
